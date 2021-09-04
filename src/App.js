a9752a59968fca2ff55f11aa8f6e423c12934696
Mcvean Soans <mc.soans02@gmail.com>
2021-09-04 23:24:18 +0530

    feat: redux setup for auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { HomeScreen, LoginScreen } from "./screens";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "firebase";
+import { login, logout, selectUser } from "features/userSlice";
+import "./App.css";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        console.log(user);
+        dispatch(login({
+          uid: user.uid,
+          email: user.email,
+        }));
+      } else {
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/screens/SignUpScreen/SignUpScreen.jsx
+++ src/screens/SignUpScreen/SignUpScreen.jsx
@@ -1,16 +1,48 @@
+import { useRef } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "firebase";
 import "./SignUpScreen.css";
 
 const SignUpScreen = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+      )
+      .then(user => {console.log(user)})
+      .catch(error => {
+        alert(error.message);
+      });
+  }
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then(() => { })
+      .catch(error => {
+        alert(error.message);
+      });
+  }
+
   return (
     <div className="signup-screen">
       <form>
         <h1>Sign In</h1>
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passwordRef} placeholder="Password" type="password" />
+        <button type="submit" onClick={handleSignIn}>Sign In</button>
         <h4>
           <span className="signup-screen-text-gray">New to Techieflix? </span>
-          <span className="signup-screen-link">Sign up Now</span>.
+          <span className="signup-screen-link" onClick={handleRegister}>Sign up Now</span>.
         </h4>
       </form>
     </div>
