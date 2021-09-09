ca707fce3f6589f5259a2a54b703b36ea9fc570b
Mcvean Soans <mc.soans02@gmail.com>
2021-09-09 14:03:32 +0530

    fix: minor fix

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,5 @@ import Banner from "./Banner/Banner";
 import Row from "./Row/Row";
 import Plans from "./Plans/Plans";
 import Footer from "./Footer/Footer";
-import IconBar from "./IconBar/IconBar";
 
-export { Nav, Banner, Row, Plans, Footer, IconBar };
+export { Nav, Banner, Row, Plans, Footer };
